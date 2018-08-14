a34ce2759c1080119801bd68d8e2bfccab1bcd9f
Jenny K <jkam17@gmail.com>
2018-08-14 23:01:10 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken? (board,i)
-  if board[i] == '' or board[i] == ' ' or i==nil
+  if board[i] == '' or board[i] == ' ' or board[i]==nil
     return FALSE
   else
     return TRUE
